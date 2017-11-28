860e4861972f21e0f6d3335819c68a8a12cc9b00
khushal K j <j.kushal.jain@gmail.com>
2017-11-28 14:36:56 +0530

    dummy keys added

--- kafka_producer_user_tweets.py
+++ kafka_producer_user_tweets.py
@@ -4,10 +4,10 @@ import kafka
 from kafka import KafkaProducer
 from kafka.errors import KafkaTimeoutError
 
-consumer_key = "2NWeA37a9tny7S7T6jGhWDZ5v"
-consumer_secret = "aL7QS80ECR4bKniBPnnDfDYW2mb4rlsoSi8Nb5KtDjfZU0idiE"
-access_key = "2795049142-TdE4d9OwtPBdd3V1rGHrmtPz9UocG8x2aGT1YHu"
-access_secret = "L0ygY1KY0NxePWhfok70PDGJCVvjM6nMOTeIwh3gLBqBo"
+consumer_key = "2NWeA37a9tny7S7klT6jGhWDZ5v"
+consumer_secret = "aL7QS80ECR4bKniBPnnDfDYklW2mb4rlsoSi8Nb5KtDjfZU0idiE"
+access_key = "2795049142-TdE4d9OwtPBkldd3V1rGHrmtPz9UocG8x2aGT1YHu"
+access_secret = "L0ygY1KY0NxePWhfok70PDGJCklVvjM6nMOTeIwh3gLBqBo"
 
 #creating an OAuthHandler instance
 auth=tweepy.OAuthHandler(consumer_key,consumer_secret)
